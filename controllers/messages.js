e841384dfe5fc6c66cd534a508448de8f2dfa00f
Mcswiggi <mrchitrason@gmail.com>
2021-07-22 23:02:10 -0400

    add create and index function for messages

--- controllers/messages.js
+++ controllers/messages.js
@@ -10,10 +10,22 @@ export{
 }
 
 function index (req, res){
-    
+    Message.find({})
+    .then((messages) => {
+        res.render('messages/index', {
+            messages
+        })
+    })
+    .catch((err) => {
+        res.render(err)
+    })
 }
 function create (req, res){
-
+req.body.author = req.user.profile._id
+Message.create(req.body)
+.then(() => {
+    res.redirect('/messages')
+})
 }
 function show (req, res){
 
